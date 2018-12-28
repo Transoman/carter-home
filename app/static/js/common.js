d1874b9a064bbf2f72eaa943462dd395562901ca
transoman <vasiag10@gmail.com>
2018-12-28 16:21:01 +0200

    Зверстаний перший блок з адаптивом

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -1,9 +1,27 @@
 jQuery(document).ready(function($) {
 
   // Toggle nav menu
+  var overlay = $('.overlay');
   $('.nav-toggle').on('click', function (e) {
     e.preventDefault();
+    if (overlay[0].style.opacity != 0.8) {
+      overlay.show().animate({ opacity: 0.8, }, 200);
+    }
+    else {
+      overlay.hide().animate({ opacity: 0, }, 200);
+    }
+    
     $(this).toggleClass('active');
+    $('.header__nav').toggleClass('open');
+  });
+
+  overlay.add('.nav').on('scroll touchmove mousewheel', function (event) {
+    event.preventDefault();
+  });
+
+  overlay.click(function(){
+    overlay.hide().animate({ opacity: 0, }, 200);
+    $('.nav-toggle').toggleClass('active');
     $('.header__nav').toggleClass('open');
   });
 
@@ -12,6 +30,16 @@ jQuery(document).ready(function($) {
     transition: 'all 0.3s',
     onclose: function() {
       $(this).find('label.error').remove();
+    }
+  });
+
+  // Fixed header
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 10) {
+      $('.header').addClass('fixed');
+    }
+    else {
+      $('.header').removeClass('fixed');
     }
   });
 
