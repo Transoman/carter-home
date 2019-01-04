4836d1be5b0faf3557ebd2ebf6287361401285b2
transoman <vasiag10@gmail.com>
2019-01-04 12:35:36 +0200

    Зверстав футер

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,7 @@
 module.exports = function () {
     $.gulp.task('watch', function () {
         $.gulp.watch('./app/pug/**/*.pug', $.gulp.series('pug'));
+        $.gulp.watch('./app/static/fonts/**/*.*', $.gulp.series('fonts'));
         $.gulp.watch('./app/static/sass/**/*.sass', $.gulp.series('styles:dev'));
         $.gulp.watch(['./app/static/images/general/**/*.{png,jpg,gif}',
             './app/static/images/content/**/*.{png,jpg,gif,svg}'], $.gulp.series('img:dev'));
