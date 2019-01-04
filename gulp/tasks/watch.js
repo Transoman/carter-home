90db33dd92b37ac31530e10384c4cc854d7adef6
transoman <vasiag10@gmail.com>
2019-01-04 12:57:32 +0200

    Замінив лого на svg. Поправив стилі

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ module.exports = function () {
         $.gulp.watch('./app/pug/**/*.pug', $.gulp.series('pug'));
         $.gulp.watch('./app/static/fonts/**/*.*', $.gulp.series('fonts'));
         $.gulp.watch('./app/static/sass/**/*.sass', $.gulp.series('styles:dev'));
-        $.gulp.watch(['./app/static/images/general/**/*.{png,jpg,gif}',
+        $.gulp.watch(['./app/static/images/general/**/*.{png,jpg,gif,svg}',
             './app/static/images/content/**/*.{png,jpg,gif,svg}'], $.gulp.series('img:dev'));
         $.gulp.watch('./app/static/images/svg/*.svg', $.gulp.series('svg'));
         $.gulp.watch('./app/static/js/**/*.js', $.gulp.series('js:dev'));
