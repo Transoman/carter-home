3564df623f26c4da47c727bb8123f64f57d52252
transoman <vasiag10@gmail.com>
2019-01-24 11:13:50 +0200

    Added new animation. Change parameters on whatch file gulp

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,10 +3,10 @@ module.exports = function () {
         $.gulp.watch('./app/pug/**/*.pug', $.gulp.series('pug'));
         $.gulp.watch('../carters-home/**/*.php', $.browserSync.reload);
         $.gulp.watch('./app/static/fonts/**/*.*', $.gulp.series('fonts'));
-        $.gulp.watch('./app/static/sass/**/*.sass', $.gulp.series('styles:dev'));
+        $.gulp.watch('./app/static/sass/**/*.sass', $.gulp.series('styles:build-min'));
         $.gulp.watch(['./app/static/images/general/**/*.{png,jpg,gif,svg}',
             './app/static/images/content/**/*.{png,jpg,gif,svg}'], $.gulp.series('img:dev'));
         $.gulp.watch('./app/static/images/svg/*.svg', $.gulp.series('svg'));
-        $.gulp.watch('./app/static/js/**/*.js', $.gulp.series('js:dev'));
+        $.gulp.watch('./app/static/js/**/*.js', $.gulp.series('js:build-min'));
     });
 };
